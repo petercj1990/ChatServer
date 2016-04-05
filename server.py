6e9811104b08c136a8c06b22f3fc08142809451f
Peter Jensen <petetjensen@gmail.com>
2016-04-05 15:30:29 -0700

    newest version

--- server.py
+++ server.py
@@ -1,118 +1,116 @@
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import SocketServer
+from urlparse import urlparse, parse_qs
+from os import curdir, sep
+import re
+import cgi
+# import asyncore import dispacher
 import socket
 import sys
-from _thread import *
+# from _thread import *
+PUBLIC_RESOURCE_PREFIX = '/'
+PUBLIC_DIRECTORY = './'
 
-host = ''
-port = 9020
-chatbufflen = 4096
-#set up the socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#trys to make 
+PORT_NUMBER = 8081
+
+#This class will handles any incoming request from
+#the browser 
+class myHandler(SimpleHTTPRequestHandler):
+	def translate_path(self, path):
+	    if self.path.startswith(PUBLIC_RESOURCE_PREFIX):
+	        if self.path == PUBLIC_RESOURCE_PREFIX or self.path == PUBLIC_RESOURCE_PREFIX + '/':
+	            return PUBLIC_DIRECTORY + '/index.html'
+	        else:
+	            return PUBLIC_DIRECTORY + path[len(PUBLIC_RESOURCE_PREFIX):]
+	    else:
+	    	return SimpleHTTPServer.SimpleHTTPRequestHandler.translate_path(self, path)
+	#Handler for the GET requests
+	def do_GET(self):
+		if self.path=="/chat":
+			self.path="/public/chat.html"
+		if self.path=="/chathistory":
+			self.path="/public/chathistory.txt"
+			print "im here"
+
+		if re.search("^/SEND",self.path):
+			values = parse_qs(urlparse(self.path).query)
+			username = values['username'][0]
+			message = values['message'][0]
+			if username == "":
+				username = "Guest"
+			if message == "":
+				message = "no message man"
+            #sep separator, accounts for backslash or forward slash depending on os
+			fHandle = open(curdir + sep + "public/chathistory.txt", "a")  
+			fHandle.write("<span>" + username + ":</span>" + message + "<br>\n")
+			fHandle.close()
+			self.path = "./public/chat.html"
+
+		try:
+			#Check the file extension required and
+			#set the right mime type
+
+			sendReply = False
+			if self.path.endswith(".html"):
+				mimetype='text/html'
+				sendReply = True
+			if self.path.endswith(".txt"):
+				mimetype='text/plain'
+				sendReply = True
+			if self.path.endswith(".jpg"):
+				mimetype='image/jpg'
+				sendReply = True
+			if self.path.endswith(".gif"):
+				mimetype='image/gif'
+				sendReply = True
+			if self.path.endswith(".js"):
+				mimetype='application/javascript'
+				sendReply = True
+			if self.path.endswith(".css"):
+				mimetype='text/css'
+				sendReply = True
+			if sendReply == True:
+				#Open the static file requested and send it
+				f = open(curdir + sep + self.path) 
+				self.send_response(200)
+				self.send_header('Content-type',mimetype)
+				self.end_headers()
+				self.wfile.write(f.read())
+				f.close()
+			return
+
+		except IOError:
+			self.send_error(404,'File Not Found: %s' % self.path)
+
+	#Handler for the POST requests
+	def do_POST(self):
+		if self.path=="/send":
+			form = cgi.FieldStorage(
+				fp=self.rfile, 
+				headers=self.headers,
+				environ={'REQUEST_METHOD':'POST',
+		                 'CONTENT_TYPE':self.headers['Content-Type'],
+			})
+
+			print "Your name is: %s" % form["your_name"].value
+			self.send_response(200)
+			self.end_headers()
+			self.wfile.write("Thanks %s !" % form["your_name"].value)
+			return			
+			
+			
 try:
-	s.bind((host,port))
-except socket.error as e:
-	print(str(e))
+	#Create a web server and define the handler to manage the
+	#incoming request
+	server = SocketServer.TCPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
 
-s.listen(5)
-print('waiting for connection \n')
-# establishes connection
-def threaded_client(conn):
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
+	
 
-	name = 'unknown'
-	chatbuff = []
-	yaboi = "Welcome to "+ name + "'s chat room\r\n"
-	conn.send(str.encode( yaboi ))
-#while connection is open reacts to clients commands
-	stay = True;
-	reply = ""
-	while stay:
-		# print( "\n")
-		data=conn.recv(chatbufflen)
-		reply=data.decode('utf-8')
-		# print("|"+ reply + "|")
-		# if reply.endswith('\r\n'):
-		# 	reply = reply[0,-2]
-		# print(reply + "\r\n")
-		enc = str.encode(reply)
-		
-		if not data:
-			break
-		if reply.startswith('help'):
-			# print('help' )
-			reply = ""
-			helpret = """Client request \"help<cr><lf>\" receives a response of a list of the commands and their syntax.\n
-		Client request \"test: words<cr><lf>\"  receives a response of \"words<cr><lf>\".
-		Client request \"name: <chatname><cr><lf>\" receives a response of \"OK<cr><lf>\".
-		Client request \"get<cr><lf>\" receives a response of the entire contents of the chat buffer.
-		Client request \"push: <stuff><cr><lf>\" receives a response of \"OK<cr><lf>\".  The result is that \"<chatname>: <stuff>\" is added as a new line to the chat buffer.
-		Client request \"getrange <startline> <endline><cr><lf>\" receives a response of lines <startline> through <endline> from the chat buffer.Client request \"adios<cr><lf>\" will quit the current connection\r\n
-		"""
-			conn.send(str.encode(helpret[0:-3]))
-		elif reply.startswith('adios'):
-			# print('adios' 
-			stay = False;
-			conn.close()
-		elif reply.startswith('test: '):
-			# print("test" )
-			words = reply[6:-2]
-			# print("|" + words + "|")
-			conn.send(str.encode(words+'\r\n'))
-		elif reply.startswith('name: '):
-			# print('name')
-			name = reply[6:-2]
-			# print("|" + name + "|")
-			conn.send(str.encode('OK\r\n'))
-		elif reply.startswith('time'):
-			import time
-			ts = time.time()
-			import datetime
-			st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-			conn.send(str.encode(st+'\r\n'))
-		elif reply.startswith('getrange: '):
-			print('getrange')
-			numbers = reply.split(' ', 1)[1]
-			one = int(numbers.split(' ', 1)[0])
-			two = int(numbers.split(' ', 1)[1])
-			# print("range 1:" + str(one) + " 2 " + str(two))
-			if one < 0 or one >= len(chatbuff):
-				conn.send(str.encode('Out of Range\r\n'))
-			elif two < 0 or two >= len(chatbuff):
-				conn.send(str.encode('Out of Range\r\n'))
-			else:
-				chat_ranged = ""
-				for i in range(one, two):
-					print('your stuff is' + i)
-					chat_ranged= chat_ranged + chatbuff[i] + '\n'
-				conn.send(str.encode(chat_ranged+'\r\n'))
-				chat_ranged = ""
-
-		elif reply.startswith('push: '):
-			# print('push')
-			import time
-			ts = time.time()
-			import datetime
-			st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-			stuff = reply[6:-2]
-			chatbuff.append(name+': '+stuff+"\t" + st)
-			# print("|" + chatbuff[len(chatbuff) -1] + "|")
-			conn.send(str.encode('OK\r\n'))
-		elif reply.startswith('get'):
-			# print('get')
-			chat_ranged = ""
-			for i in chatbuff:
-				chat_ranged= chat_ranged + i + "\n"
-			chat_ranged = chat_ranged + "\r\n"
-			conn.send(str.encode(chat_ranged))
-		else:
-			# print("error")
-			conn.send(str.encode( "Error: unrecognized command: "+reply+"\r\n"))
-
-	#what closes the connection
-	reply = ""
-	conn.close()
-# shows what is happening on server side
-while True:
-	conn, addr = s.accept()
-	print('connected to: ' + addr[0] + ':' + str(addr[1]))
-	start_new_thread(threaded_client,(conn,))
